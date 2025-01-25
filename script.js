6da6603f7eff2db7823c7865120c5bedf27c82a7
husate <wahhhwahhwahhh@gmail.com>
2025-01-25 02:13:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let button = document.querySelector('button');
-
+let clicked = false;
 button.onclick = function () {
+  if (clicked == false){
   button.style.backgroundColor = 'red';
   button.textContent = 'You clicked me!';
+  }
+  else if (clicked == true){
+  button.style.backgroundColor = 'green';
+  button.textContent = 'this is a button';
+  }
 }
