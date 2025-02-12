9fd32797db4fd6b97ba05a15e6436ccd1990aaec
Husate <wahhhwahhwahhh@gmail.com>
2025-02-11 21:27:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const button = document.querySelector('button');
 const askNameButton = document.querySelector('#ask button');
+const header = document.querySelector('#header1');
 let clicked = false;
 button.onclick = function () {
-  if (clicked == false){
+  if (!clicked){
   button.style.backgroundColor = 'red';
   button.textContent = 'You clicked me!';
   clicked = true;
   }
-  else if (clicked == true){
+  else if (clicked){
   button.style.backgroundColor = 'green';
   button.textContent = 'this is a button';
   clicked = false;
   }
+}
+
+askNameButton.onClick = function () {
+  let name = prompt("What is your name?");
+  header.textContent = `Hello, ${name}!`;
 }
